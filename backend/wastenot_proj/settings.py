af6b84da0c9c3fdd24cce25d9d4fff50c34ab3a4
interdimensionalpizza <michaellake05@gmail.com>
2023-08-28 13:23:15 -0700

    added .env

--- backend/wastenot_proj/settings.py
+++ backend/wastenot_proj/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import dotenv_values
+env = dotenv_values(".env")
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +22,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-h#f!^qkhuv5$lf6u&w%a*rpoab@q6xpk^c81r-8s_ct-36jt20'
+
+SECRET_KEY = env.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
