46ce9d54f4ddd9ce01a310ce8401c22387f27a17
interdimensionalpizza <michaellake05@gmail.com>
2023-08-29 15:08:04 -0700

    continued flushing out views for pantryitem

---
+++ backend/pantryitem_app/serializers.py
@@ -0,0 +1,7 @@
+from rest_framework.serializers import ModelSerializer
+from .models import PantryItem
+
+class PantryItemSerializer(ModelSerializer):
+    class Meta:
+        model = PantryItem
+        fields = ['id', 'item_name']
