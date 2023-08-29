0b44eda696bb6a1b0472ea1575e40bfec028a6d9
Ivan Goudyrev <ivan.goudyrev@gmail.com>
2023-08-29 16:59:05 -0400

    initial views and urls for pantryitem

--- backend/pantryitem_app/views.py
+++ backend/pantryitem_app/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+# from rest_framework.views import APIView
+from user_app.views import User_permissions
+from rest_framework.response import Response
 
 # Create your views here.
+class All_pantry_items(User_permissions): #api/pantryitem/
+    def get(self, request):
+        items = request.user.pantryitems.all()
+        pass
+
+
+    def post(self, request):
+        pass
+
+class A_pantry_item(User_permissions): #api/pantryitem/{id}/
+    def get(self, request, id):
+        pass
+
+    def put(self, request, id):
+        pass
+
+    def delete(self, request, id):
+        pass
