771c9771bcd107bdab61d0e41e3cbc570fcd15e2
Kelvin Lau <crosby2987@gmail.com>
2023-09-03 21:07:47 -0500

    Strict recipe search complete. Loading takes 10-15 seconds due to query to OpenAI

--- backend/openai_app/urls.py
+++ backend/openai_app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import GPT3RecipeView
+from .views import GPT3RecipeView, search_recipe_image
 
 urlpatterns = [
     path("", GPT3RecipeView.as_view(), name="chatGPT_query"),
+    path("image/<str:recipe_name>/", search_recipe_image, name="search_image")
 ]
